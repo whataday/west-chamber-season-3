8987b7a5fac636e0d3f57d530dc93b93fde9f36b
liruqi <liruqi@gmail.com>
2011-08-01 19:52:23 +0800

    add gfw - clean up dns

--- client.sh
+++ client.sh
@@ -8,3 +8,6 @@ fi
 ipset -R < CHINA
 
 iptables -A INPUT -p tcp -m tcp --tcp-flags RST RST -m set ! --match-set CHINA src -j DROP
+
+# you need to compile west-chamber from http://code.google.com/p/scholarzhang in advance
+iptables -A INPUT -p udp -m udp --sport 53 -m state --state ESTABLISHED -m gfw -j DROP
